566ed20f646742d94b6a23103ca45f7079d2d481
JoaoNormando <joaonormando50@gmail.com>
2017-12-29 12:56:58 -0200

    Criação da classe usuário

---
+++ src/app/usuarios/shared/usuario.model.ts
@@ -0,0 +1,13 @@
+export class UsuarioModel {
+
+    id: number;
+    nome: string;
+    sobrenome: string;
+    telefone: string;
+    celular: string;
+    email: string;
+    emailAcesso: string;
+    senha: string;
+    observacao: string;
+
+}
